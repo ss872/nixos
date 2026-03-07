cc3bca693bf62ba0e36b8252d636e80ab96b1230
Shyamal Shah <shah.shyamal.872@gmail.com>
2026-03-07 16:41:32 -0600

    boot added tweeks

--- os/desktop/gnome.nix
+++ os/desktop/gnome.nix
@@ -1,4 +1,11 @@
+{ pkgs, ... }:
+
 {
   # GNOME desktop.
   services.desktopManager.gnome.enable = true;
+
+  # GNOME Tweaks for advanced desktop customization.
+  environment.systemPackages = with pkgs; [
+    gnome-tweaks
+  ];
 }
